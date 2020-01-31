9c057100dcbde0f9e2335b312f91ccc6a70270c6
Nikita Gryzlov <nixel2007@gmail.com>
2020-01-31 17:45:46 +0300

    Штатный энтрипоинт для запуска в дженкинсе

---
+++ base-jenkins-agent/docker-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/init &
+wget -O agent.jar $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_JAR_URL && java -jar agent.jar -jnlpUrl $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_JNLP_URL -secret $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_SECRET -noReconnect -workDir /home/jenkins
