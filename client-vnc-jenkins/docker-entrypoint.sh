9dd35c6b00db53826c23e665a815bf4a00f6ac58
Nikita Gryzlov <nixel2007@gmail.com>
2020-02-10 11:35:53 +0300

    Запуск /init если файл существует

--- client-vnc-jenkins/docker-entrypoint.sh
+++ client-vnc-jenkins/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-/init &
+if [ -f "/init" ]; then
+    /init &
+fi
+
 wget -O agent.jar $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_JAR_URL && java -jar agent.jar -jnlpUrl $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_JNLP_URL -secret $DOCKER_SWARM_PLUGIN_JENKINS_AGENT_SECRET -noReconnect -workDir /home/jenkins
