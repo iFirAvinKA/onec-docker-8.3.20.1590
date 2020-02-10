23bd09fc4f521365a0dd61d51412a256738b7ea4
Nikita Gryzlov <nixel2007@gmail.com>
2020-02-10 17:23:21 +0300

    Прототип сборки сервера хранилища с апачем

---
+++ crs-apache/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# start crserver
+exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8
+
+# start apache2
+. /etc/apache2/envvars
+exec /usr/sbin/apache2 -DFOREGROUND
