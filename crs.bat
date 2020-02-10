ba5d3406af8ac786cdb6c378fd9db8318a7c8a3b
Nikita Gryzlov <nixel2007@gmail.com>
2020-02-10 11:58:38 +0300

    Revert "Установка из 64-битного установщика"
    This reverts commit 10f8be453e9b53bc1403c98942d6881f43ae7a0e.

--- crs.bat
+++
@@ -1,7 +0,0 @@
-docker build ^
-  --build-arg ONEC_USERNAME=%ONEC_USERNAME% ^
-  --build-arg ONEC_PASSWORD=%ONEC_PASSWORD% ^
-  --build-arg ONEC_VERSION=%ONEC_VERSION% ^
-  --build-arg DOCKER_USERNAME=%DOCKER_USERNAME% ^
-  -t %DOCKER_USERNAME%/crs:%ONEC_VERSION% ^
-  -f crs/Dockerfile .
