47c3353bee20635b7f9100d8098bfab4999a0f6e
Nikita Gryzlov <nixel2007@gmail.com>
2020-02-10 17:31:10 +0300

    Запуск crserver в background

--- crs-apache/docker-entrypoint.sh
+++ crs-apache/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # start crserver
-exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8
+exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8 &
 
 # start apache2
 . /etc/apache2/envvars
