c6ca70f419f4c05c70257bfe90788026898de170
Nikita Gryzlov <nixel2007@gmail.com>
2020-02-12 16:54:41 +0300

    Перенос корня сервера хранилища

--- crs-apache/docker-entrypoint.sh
+++ crs-apache/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start crserver
-exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8 -daemon &
+exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
 
 # start apache2
 source /etc/apache2/envvars
