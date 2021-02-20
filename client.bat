7cc47e94a55a286681357f3ee829ab11869464b6
Nikita Gryzlov <nixel2007@gmail.com>
2021-02-20 14:54:06 +0300

    Откат смены имени собираемого образа

--- client.bat
+++ client.bat
@@ -4,5 +4,5 @@ docker build ^
   --build-arg ONEC_PASSWORD=%ONEC_PASSWORD% ^
   --build-arg ONEC_VERSION=%ONEC_VERSION% ^
   --build-arg DOCKER_USERNAME=%DOCKER_USERNAME% ^
-  -t %DOCKER_USERNAME%/client:%ONEC_VERSION% ^
+  -t %DOCKER_USERNAME%/onec-client:%ONEC_VERSION% ^
   -f client/Dockerfile .
