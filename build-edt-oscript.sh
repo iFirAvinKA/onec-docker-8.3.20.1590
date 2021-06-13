226786ee1baf8cc79ea660b56193ff0366d68536
Nikita Gryzlov <nixel2007@gmail.com>
2021-06-13 20:28:46 +0300

    Добавлены скрипты сборки

---
+++ build-edt-oscript.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD $DOCKER_USERNAME
+
+if [ $DOCKER_SYSTEM_PRUNE = 'true' ] ; then
+    docker system prune -af
+fi
+
+last_arg='.'
+if [ $NO_CACHE = 'true' ] ; then
+	last_arg='--no-cache .'
+fi
+
+edt_version=$EDT_VERSION
+edt_escaped="${edt_version// /_}"
+
+docker build \
+    --pull \
+    --build-arg ONEC_USERNAME=$ONEC_USERNAME \
+    --build-arg ONEC_PASSWORD=$ONEC_PASSWORD \
+    --build-arg EDT_VERSION="$EDT_VERSION" \
+    -t $DOCKER_USERNAME/edt:$edt_escaped \
+    -f edt/Dockerfile \
+    $last_arg
+
+docker build \
+    --build-arg DOCKER_USERNAME=$DOCKER_USERNAME \
+    --build-arg BASE_IMAGE=edt \
+    --build-arg BASE_TAG=$edt_escaped \
+    -t $DOCKER_USERNAME/edt-agent:$edt_escaped \
+	-f jenkins-agent/Dockerfile \
+    $last_arg
+
+docker push $DOCKER_USERNAME/edt-agent:$edt_escaped
+
+docker build \
+	--pull \
+    $no_cache_arg \
+	--build-arg DOCKER_USERNAME=library \
+    --build-arg BASE_IMAGE=adoptopenjdk \
+    --build-arg BASE_TAG=14-hotspot \
+    -t $DOCKER_USERNAME/oscript-jdk:latest \
+	-f oscript/Dockerfile \
+    $last_arg
+
+docker build \
+    $no_cache_arg \
+	--build-arg DOCKER_USERNAME=$DOCKER_USERNAME \
+    --build-arg BASE_IMAGE=oscript-jdk \
+    --build-arg BASE_TAG=latest \
+    -t $DOCKER_USERNAME/oscript-agent:latest \
+	-f jenkins-agent/Dockerfile \
+    $last_arg
+
+docker push $DOCKER_USERNAME/oscript-agent:latest
--- push-all.bat
+++
@@ -1,4 +0,0 @@
-docker push registry.dev.pulsar.ru/istorozhuk/kubernetes-cluster/onec-client:%ONEC_VERSION%
-docker push registry.dev.pulsar.ru/istorozhuk/kubernetes-cluster/onec-client-vnc:%ONEC_VERSION%
-docker push registry.dev.pulsar.ru/istorozhuk/kubernetes-cluster/onec-client-vnc-jenkins:%ONEC_VERSION%
-docker push registry.dev.pulsar.ru/istorozhuk/kubernetes-cluster/base-jenkins-agent:%ONEC_VERSION%
